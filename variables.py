b8fe5b83e7329fa951006b2c294412670f6b7bb2
baxterr02 <bukhari.bakht@gmail.com>
2025-07-31 14:41:47 +0800

    Latest Commit

---
+++ variables.py
@@ -0,0 +1,17 @@
+first_name = "Bax"
+food = "Pizza"
+age = 22
+email = "bakht@example.com"
+bank_account = 0.000
+is_student = True
+for_sale = True
+print(f"Hello, My name is {first_name}")
+print(f"I am {age} years old")
+print(f"I like {food}")
+print(f"My email is {email}")
+print(f"Am I a student?: {is_student}")
+print(f"My bank account has {bank_account} dollars in it")
+if for_sale:
+    print("Am I for sale?: YES!")
+else:
+    print("Am I for sale?: NO!")
