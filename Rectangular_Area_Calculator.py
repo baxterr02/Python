2b98f0ba2cd5cd8dae85a7b388846b07f2bc5f81
baxterr02 <bukhari.bakht@gmail.com>
2025-07-31 19:00:36 +0800

    Latest Commit

---
+++ Rectangular_Area_Calculator.py
@@ -0,0 +1,6 @@
+#User Input Exercise 1
+length = int(input("Enter the length(cm): "))
+width = int(input("Enter the width(cm): "))
+area = length * width
+
+print(f"The area of the rectangle is: {area}cm")
---
+++ typecasting.py
@@ -0,0 +1,8 @@
+name = "Bax"
+age = 22
+gpa = 3.2
+is_student = True
+
+age = str(age)
+age += "1"
+print(age)
